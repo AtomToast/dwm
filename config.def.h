40a2c5208b27e1aef57cf41a3b7b86a4ad997661
AtomToast <reg-2@t-online.de>
2020-01-04 15:37:54 +0100

    make only gimps starting dialogue floating

--- config.def.h
+++ config.def.h
@@ -27,7 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     NULL,       "GIMP Startup",       0,            1,           -1 },
 	{ "firefox",  NULL,       NULL,       1<<1,         0,           -1 },
 	{ "discord",  NULL,       NULL,       1<<8,         0,           -1 },
 	{ "TelegramDesktop",  NULL,       NULL,       1<<7,         0,           -1 },
