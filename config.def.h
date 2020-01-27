f63f730fba274789d44ed92dd1430738a8d538cb
AtomToast <reg-2@t-online.de>
2020-01-27 19:56:37 +0100

    reset font config to Hack Nerd Font Mono

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:pixelsize=13:antialias=true:autohint=true" };
-static const char dmenufont[]       = "monospace:pixelsize=13:antialias=true:autohint=true";
+static const char *fonts[]          = { "Hack Nerd Font Mono:pixelsize=13:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Hack Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
 static const char col_gray1[]       = "#282828"; // "#222222"
 static const char col_gray2[]       = "#49483e"; // "#444444"
 static const char col_gray3[]       = "#a1efe4"; // "#bbbbbb"
