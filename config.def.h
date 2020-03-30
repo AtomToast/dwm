48b745a8ad286596c7be25a4698d396c9674e37f
AtomToast <reg-2@t-online.de>
2020-03-30 13:01:24 +0200

    add color emoji support because why

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true", "Joy Pixels:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true";
 static const char col_gray1[]       = "#282828"; // "#222222"
 static const char col_gray2[]       = "#49483e"; // "#444444"
