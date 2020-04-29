3a7f04dc416927e2af65ae895f98704d9b09bd13
AtomToast <reg-2@t-online.de>
2020-04-29 13:18:03 +0200

    open QjackCtl in floating mode

--- config.h
+++ config.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	{ "firefox",            NULL,       NULL,                   1<<1,         0,           -1 },
 	{ "discord",            NULL,       NULL,                   1<<8,         0,           -1 },
 	{ "TelegramDesktop",    NULL,       NULL,                   1<<7,         0,           -1 },
+	{ "QjackCtl",           NULL,       NULL,                   0,            1,           -1 },
 };
 
 /* layout(s) */
